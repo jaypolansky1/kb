bba327f887a280d7dd887fdd247c099df43a0a35
jaypolansky1 <jaypolansky@gmail.com>
2026-07-19 01:06:01 -0400

    Update editUrl in docusaurus.config.js.

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -51,6 +51,7 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+           exclude: ['tutorial-basics/**', 'tutorial-extras/**', 'intro.mdx'],
         },
         blog: {
           showReadingTime: true,
