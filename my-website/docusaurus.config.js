e5b24519c9f39277fede868568ef229c4bdffd05
jaypolansky1 <jaypolansky@gmail.com>
2026-07-21 23:33:55 -0400

    Update production URL in Docusaurus config

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://jaypolansky1.github.io',
+  url: 'https://docs.chasethesplit.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
