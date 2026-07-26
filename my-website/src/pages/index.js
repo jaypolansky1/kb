52efc77766c13a42de40b678e0d0f862fb9865cc
jaypolansky1 <jaypolansky@gmail.com>
2026-07-25 23:39:25 -0400

    Update documentation link to new URL

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/getting-started/connect-to-concept2-logbook">
+            to="https://docs.chasethesplit.com/docs/">
             Read The Docs
           </Link>
         </div>
